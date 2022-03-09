5a626130bcf75157b25f4da7e549605a192fd670
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-09 17:38:22 -0300

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#landing'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'onboarding', to: 'pages#onboarding'
+
+  resources :users, only: :show
+
+  resources :pets do
+    resources :tag_readings, except: [:edit, :update, :destroy]
+  end
+
+  resources :notifications, only: [:index]
 end
