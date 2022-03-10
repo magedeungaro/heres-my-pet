26e0ba94e12c89f2bde8fa1ed8cebe06d0d3753c
Paulo Bazilio <pvbazilio@gmail.com>
2022-03-10 12:28:19 -0300

    create contoller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,5 +7,16 @@ class PetsController < ApplicationController
   end
 
   def create
+    @pet = Pet.new(pet_params)
+    @user = User.find(params[:user_id])
+    @pet.user = @user
+    authorize @pet
+    @pet.save
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:user_id, :name, :photo, :sex, :birthdate, :spayed, :additional_info, :special_treatment, :lost, :pet_type)
   end
 end
