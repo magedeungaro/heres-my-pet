84df6e8b6275a77352a7dc162279b4222106b28f
Thiago Calazans <thiagotorrescalazans@gmail.com>
2022-03-10 14:43:26 -0300

    user show page update

---
+++ app/helpers/image_helper.rb
@@ -0,0 +1,14 @@
+module ImageHelper
+  def background_image(args = {})
+    url = 'https://img.freepik.com/free-vector/pet-veterinary_24908-57965.jpg?size=338&ext=jpg&ga=GA1.2.867202971.1638403200'
+
+    url = args[:url] unless args[:url].nil?
+
+    url = cl_image_path url if args[:cloudinary]
+
+    color_one = 'rgba(0, 0, 0, 0)'
+    color_two = 'rgba(0, 0, 0, .1)'
+
+    "background-image: linear-gradient(#{color_one}, #{color_two}), url(#{url})"
+  end
+end
