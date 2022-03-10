2b42bc3e00f912581245b2205a812b2a946c1961
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-10 12:01:27 -0300

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'pages#landing'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'onboarding', to: 'pages#onboarding', as: :onboarding
-  get 'ui-kitt', to: 'pages#ui_kitt'
+  get 'ui-kitt', to: 'pages#ui_kitt', as: :ui
 
   resources :users, only: :show
 
