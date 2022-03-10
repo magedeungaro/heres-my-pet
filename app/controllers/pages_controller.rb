779018901b1430fefcddcace5a50be659853b3b6
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-10 11:49:26 -0300

    simple form for

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :landing
+  skip_before_action :authenticate_user!, only: [:landing, :ui_kitt]
 
   def landing
   end
