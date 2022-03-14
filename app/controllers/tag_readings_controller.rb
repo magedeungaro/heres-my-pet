d8b259ffde07aa388d2fc34efde878f3feb72bd6
Paulo Bazilio <pvbazilio@gmail.com>
2022-03-14 13:09:05 -0300

    MVC tag_reading done

--- app/controllers/tag_readings_controller.rb
+++ app/controllers/tag_readings_controller.rb
@@ -1,21 +1,27 @@
 class TagReadingsController < ApplicationController
 
   def index
-    @tag_readings = TagReading.all
+    @pet = Pet.find(params[:pet_id])
+    @tag_readings = policy_scope(TagReading).order(created_at: :DESC).limit(10)
   end
 
   def show
+    @pet = Pet.find(params[:pet_id])
     @tag_reading = TagReading.find(params[:id])
+    authorize @tag_reading
   end
 
   def new
+    @pet = Pet.find(params[:pet_id])
     @tag_reading = TagReading.new
+    authorize @tag_reading
 
   end
 
   def create
     @pet = Pet.find(params[:pet_id])
     @tag_reading = TagReading.new(tag_reading_params)
+    authorize @tag_reading
     @tag_reading.pet = @pet
     @tag_reading.save
     if @tag_reading.save
