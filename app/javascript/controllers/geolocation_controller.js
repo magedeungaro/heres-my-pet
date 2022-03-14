7a7551c7b7ac790e7c270218abcb29844b44f516
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-14 20:40:55 -0300

    GOT RID OF LEFT ASSIGNMENTTTTTT

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -4,14 +4,12 @@ export default class extends Controller {
   static targets = [ "lat", "lng" ]
 
   connect() {
-    console.log(this.latTarget)
   }
   shareLocation() {
-    console.log("Test 2")
-    navigator.geolocation.getCurrentPosition((position) =>{
-      let lat = position.coords.latitude;
-      let long = position.coords.longitude;
-      console.log(this);
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude
+      let long = position.coords.longitude
+
       this.latTarget.value = lat
       this.lngTarget.value = long
     });
