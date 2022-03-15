fa39fa3facb3d27ee268c64c14133d1aecba0bf3
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-15 14:34:55 -0300

    user page navbar

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
   def render_navbar
-    if user_signed_in? && !current_page?(ui_path)
+    if user_signed_in? && !current_page?(ui_path) && !@pet && !current_page?(user_path(current_user))
       render 'shared/bottom_navbar'
     end
   end
