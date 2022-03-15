925c19c131d2671daf7a5ec78edfb459313d250a
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-15 00:43:09 -0300

    redirect user to dashboard if logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:landing, :ui_kitt]
 
   def landing
+    redirect_to pets_path if user_signed_in?
   end
 
   def onboarding
