a3b245c383b08fdfd6d3793934d63ac445cf9769
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-21 15:35:25 -0300

    waves positioned

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
   def render_navbar
-    if user_signed_in? && ((!current_page?(ui_path) && !@pet && !current_page?(user_path(current_user))) || @tag_reading)
+    if user_signed_in? && ((!current_page?(ui_path) && !@pet && !current_page?(user_path(current_user)) && !current_page?(onboarding_path)) || @tag_reading)
       render 'shared/bottom_navbar'
     end
   end
