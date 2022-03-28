f5d610e319eeb27016a368ed5e024d433e766299
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-28 18:03:45 -0300

    api caller service with rest client

--- app/services/api_caller_service.rb
+++ app/services/api_caller_service.rb
@@ -2,6 +2,10 @@ require 'rest-client'
 
 class ApiCallerService
   def self.fetch(verb: :get, url:, body: nil, headers: nil, **rest)
-    RestClient::Request.new(method: verb, url: url, body: body, headers: headers).execute
+    begin
+      RestClient::Request.new(method: verb, url: url, body: body, headers: headers).execute
+    rescue StandardError => e
+      puts e
+    end
   end
 end
