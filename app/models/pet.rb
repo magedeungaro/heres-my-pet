47dd5f42817517408728dd5a9e5fef45e3252d3a
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-03-28 22:58:56 -0300

    file attachment working

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,15 +2,9 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :tag_readings, dependent: :destroy
   has_one_attached :photo
+  has_one_attached :qr_code
 
   validates :name, presence: true
   validates :gender, presence: true
   validates :pet_type, presence: true
-
-  private
-
-  def method_name
-
-  end
-
 end
--- app/services/file_service.rb
+++ app/services/file_service.rb
@@ -3,7 +3,7 @@ class FileService
     IO.binwrite("tmp/#{name}.png", file.to_s)
   end
 
-  def create_blob(file:, file_name:, file_extension: 'png')
+  def self.create_blob(file:, file_name:, file_extension: 'image/png')
     ActiveStorage::Blob.create_after_upload!(
       io: File.open(file),
       filename: file_name,
@@ -11,10 +11,20 @@ class FileService
     )
   end
 
-  def self.attach_file(file:, file_name:, obj:, attachble_type:, file_extension: 'png')
-    tmp_file = self.create_tmp_file(name: file_name, file: file)
-    blob = self.create_blob(file: tmp_file, file_name: file_name, file_extension: file_extension)
+  def self.attach_file(file:, file_name:, obj:, attachable_type:, file_extension: 'image/png')
 
-    obj[attachble_type.underscore.constantize].attach(blob)
+    unless obj.respond_to? attachable_type.underscore
+      message = 'Attachable type not found on model provided.'
+      Sentry.capture_exception(NoMethodError.new(message))
+
+      return message
+    end
+
+
+    tmp_file = self.create_tmp_file(name: file_name, file: file)
+    blob = self.create_blob(file: "tmp/#{file_name}.png", file_name: file_name, file_extension: file_extension)
+
+    puts "Sent #{file_name} to Cloudinary"
+    obj.send(attachable_type.underscore).attach(blob)
   end
 end
