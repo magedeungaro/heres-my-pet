cfff6abd93e78bb08906026aa0425628b27e5b78
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-22 19:36:25 -0300

    changed name to file_name to be more semantic

--- app/services/file_service.rb
+++ app/services/file_service.rb
@@ -1,6 +1,6 @@
 class FileService
-  def self.create_tmp_file(name:, file_content:, file_extension: 'png')
-    IO.binwrite("tmp/#{ name }.#{ file_extension }", file_content.to_s)
+  def self.create_tmp_file(file_name:, file_content:, file_extension: 'png')
+    IO.binwrite("tmp/#{ file_name }.#{ file_extension }", file_content.to_s)
   end
 
   def self.create_blob(file_path:, file_name:, file_extension: 'png')
@@ -21,7 +21,7 @@ class FileService
     end
 
 
-    tmp_file = self.create_tmp_file(name: file_name, file_content: file_content, file_extension: file_extension)
+    tmp_file = self.create_tmp_file(file_name: file_name, file_content: file_content, file_extension: file_extension)
     blob = self.create_blob(file_path: "tmp/#{ file_name }.#{ file_extension }", file_name: file_name, file_extension: file_extension)
 
     puts "Sent #{file_name} to Cloudinary"
