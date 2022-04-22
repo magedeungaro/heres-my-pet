510caa13700c385c5645d8a5e5a17d2be140e9df
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-22 20:00:15 -0300

    edited the tests.sh script to loop through spec folder and run tests

--- tests.sh
+++ tests.sh
@@ -2,11 +2,11 @@
 
 if test -z "$1"
   then
-    echo 'running test system'
-    bundle exec rspec spec/system
-
-    echo 'running test services'
-    bundle exec rspec spec/services
+    shopt -s dotglob
+    find spec/ -prune -type d | while IFS= read -r d; do
+        echo "running test $d"
+        bundle exec rspec $d
+    done
   else
     echo "running test $1"
     bundle exec rspec spec/$1
