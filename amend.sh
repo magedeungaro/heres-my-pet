a1b3ecf76002d78fa127954edcdb93091c04691d
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-22 20:28:12 -0300

    amend.sh script so I don't forget to pull next time I amend a commit...

---
+++ amend.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+reset=`tput sgr0`
+magenta=`tput setaf 5`
+blue=`tput setaf 4`
+gray=`tput setaf 7`
+
+start_time=$(date +%s.%3N)
+git add $1 .
+
+git commit -amend
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+echo "${blue}Enter remote name ${gray}(default: origin)${reset}"
+read remote
+
+if test -z "$remote"
+  then
+        git pull -u origin $branch
+  else
+        git pull -u $remote $branch
+fi
+
+end_time=$(date +%s.%3N)
+elapsed=$(echo "scale=3; $end_time - $start_time" | bc)
+echo "${magenta}$(tput bold)Done in ${elapsed}ms ${reset}"
