de486ca96b784162ebe6d919adf4d58648a3f15b
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-22 19:33:48 -0300

    changed file to file_content to be more semantic

--- app/services/file_service.rb
+++ app/services/file_service.rb
@@ -1,6 +1,6 @@
 class FileService
-  def self.create_tmp_file(name:, file:, file_extension: 'png')
-    IO.binwrite("tmp/#{ name }.#{ file_extension }", file.to_s)
+  def self.create_tmp_file(name:, file_content:, file_extension: 'png')
+    IO.binwrite("tmp/#{ name }.#{ file_extension }", file_content.to_s)
   end
 
   def self.create_blob(file_path:, file_name:, file_extension: 'png')
@@ -11,7 +11,7 @@ class FileService
     )
   end
 
-  def self.attach_file(file:, file_name:, obj:, attachable_type:, file_extension: 'png')
+  def self.attach_file(file_content:, file_name:, obj:, attachable_type:, file_extension: 'png')
 
     unless obj.respond_to? attachable_type.underscore
       message = 'Attachable type not found on model provided.'
@@ -21,7 +21,7 @@ class FileService
     end
 
 
-    tmp_file = self.create_tmp_file(name: file_name, file: file, file_extension: file_extension)
+    tmp_file = self.create_tmp_file(name: file_name, file_content: file_content, file_extension: file_extension)
     blob = self.create_blob(file_path: "tmp/#{ file_name }.#{ file_extension }", file_name: file_name, file_extension: file_extension)
 
     puts "Sent #{file_name} to Cloudinary"
