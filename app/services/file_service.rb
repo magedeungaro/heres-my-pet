21a2305fd22eedf1165c781216a6384893b9bba0
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-22 18:43:14 -0300

    style fix in fileservice rb

--- app/services/file_service.rb
+++ app/services/file_service.rb
@@ -1,6 +1,6 @@
 class FileService
   def self.create_tmp_file(name:, file:, file_extension: 'png')
-    IO.binwrite("tmp/#{name}.#{file_extension}", file.to_s)
+    IO.binwrite("tmp/#{ name }.#{ file_extension }", file.to_s)
   end
 
   def self.create_blob(file:, file_name:, file_extension: 'png')
@@ -22,7 +22,7 @@ class FileService
 
 
     tmp_file = self.create_tmp_file(name: file_name, file: file, file_extension: file_extension)
-    blob = self.create_blob(file: "tmp/#{file_name}.#{file_extension}", file_name: file_name, file_extension: file_extension)
+    blob = self.create_blob(file: "tmp/#{ file_name }.#{ file_extension }", file_name: file_name, file_extension: file_extension)
 
     puts "Sent #{file_name} to Cloudinary"
     obj.send(attachable_type.underscore).attach(blob)
