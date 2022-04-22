a539fe88556021e104119392368423a7f4be9a41
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-04-22 19:25:44 -0300

    change parameter name to be more semantic (file to file_path in FileService.create_bÃlob

--- app/services/file_service.rb
+++ app/services/file_service.rb
@@ -3,9 +3,9 @@ class FileService
     IO.binwrite("tmp/#{ name }.#{ file_extension }", file.to_s)
   end
 
-  def self.create_blob(file:, file_name:, file_extension: 'png')
+  def self.create_blob(file_path:, file_name:, file_extension: 'png')
     ActiveStorage::Blob.create_after_upload!(
-      io: File.open(file),
+      io: File.open(file_path),
       filename: file_name,
       content_type: file_extension,
     )
@@ -22,7 +22,7 @@ class FileService
 
 
     tmp_file = self.create_tmp_file(name: file_name, file: file, file_extension: file_extension)
-    blob = self.create_blob(file: "tmp/#{ file_name }.#{ file_extension }", file_name: file_name, file_extension: file_extension)
+    blob = self.create_blob(file_path: "tmp/#{ file_name }.#{ file_extension }", file_name: file_name, file_extension: file_extension)
 
     puts "Sent #{file_name} to Cloudinary"
     obj.send(attachable_type.underscore).attach(blob)
